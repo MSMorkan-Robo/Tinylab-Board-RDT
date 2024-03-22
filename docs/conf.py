498768ac904ac4ca702bb7ed3991d66ff3bbaa65
ozerngg <naze.ozer@robotistan.com>
2024-03-22 16:28:03 +0300

    furo theme added

--- docs/conf.py
+++ docs/conf.py
@@ -23,4 +23,9 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'pyramid'
+extensions = [
+    ...
+    'readthedocs_sphinx_theme',
+]
+
+html_theme = 'furo'
