920d441c9e8a7ee8f87bad3664e9dc7a22d49270
ozerngg <naze.ozer@robotistan.com>
2024-03-25 11:43:09 +0300

    conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -5,6 +5,9 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
 project = 'Tinylab'
 copyright = '2024, Tinylab'
 author = 'Tinylab'
