43eba5bdc672d76476a8f5ba193d6e4ac988b718
ozerngg <naze.ozer@robotistan.com>
2024-03-25 16:12:15 +0300

    theme

--- docs/conf.py
+++ docs/conf.py
@@ -13,9 +13,14 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-# Your other extensions here
-    'sphinx_rtd_theme',
-]
+extensions = []
 
-html_theme = "sphinx_rtd_theme"
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'pyramid'
