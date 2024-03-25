da7d90afef300b50b43c213328ec85bbba6575ec
ozerngg <naze.ozer@robotistan.com>
2024-03-25 11:47:46 +0300

    conf.py

---
+++ docs/conf.py
@@ -0,0 +1,21 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+project = 'Tinylab'
+copyright = '2024, Tinylab'
+author = 'Tinylab'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+# Your other extensions here
+    'sphinx_rtd_theme',
+]
+
+html_theme = "sphinx_rtd_theme"
