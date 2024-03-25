559b5e84f7a6161d70235db983659726797b430a
M. Suat Morkan <msuat.morkan@robotistan.com>
2024-03-25 17:34:29 +0300

    theme test

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['furo']
+extensions = ['sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -23,4 +23,4 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
+html_theme = 'sphinx_rtd_theme'
